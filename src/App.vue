144ad6587bf1c987ec628da9ae9dca4d5a52588d
1Zod <nathanmarrega@gmail.com>
2022-09-03 20:32:50 -0300

    Add doces e maquina pns

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="1 real" src="./assets/1real.png">
+  <img alt="2 real" src="./assets/2reais.png">
+  <img alt="5 real" src="./assets/5reais.png">
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
   }
 }
 </script>
