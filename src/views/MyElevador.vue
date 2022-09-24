5049464235fb01c8bf7e5100a724c4643bfe263b
1Zod <nathanmarrega@gmail.com>
2022-09-24 19:39:32 -0300

    Add elevador

--- src/views/MyElevador.vue
+++ src/views/MyElevador.vue
@@ -1,12 +1,14 @@
 <template>
-   <div class="background-elevador">
-        <div>
-            <button @click="terreio_ao_1" style="height: 4.5vh;">Terreo</button>
+    <div class="background-elevador">
+        <div id="principal">
+            <div class="campo_predio">
+            <img src="@/assets/img/predio.png" style="height: 99vh;">
+            </div>
+            <div class="campo_elevador">
+            <img id="elevador" src="@/assets/img/elevador.png" style="height: 99vh;">
+            </div>
         </div>
-        <dir class="videos">
-            <video id='1' src="@/assets/video/terreo_ao_1.mp4" style="height: 99vh;"></video>
-        </dir>
-   </div>
+    </div>
 </template>
 
 <script>
@@ -18,25 +20,39 @@
         };
       },
       methods:{
-        terreio_ao_1(){
-            var myvideo = document.querySelector("1")
-            myvideo.play();
-        },
+        // terreio_ao_1(){
+        //     var myvideo = document.querySelector("1")
+        //     myvideo.play();
+        // },
       }
     }
 </script>
 
 <style>
+    
+html, body {
+ overflow: hidden;
+}
 
 .background-elevador{
     margin: 0%;
-    height: 100vh;
+    height: 100%;
     width: 100%;
     background-image: radial-gradient( circle farthest-corner at 10% 20%,  rgba(90,92,106,1) 0%, rgba(32,45,58,1) 81.3% );
     /* background-repeat: repeat; */
 }
 
-.videos{
-    height: 10vh;
+#principal{
+    display: grid;
+    grid-template-columns: 1vh 1vh 1vh 1vh;
+   
+    justify-items: left;
+    column-gap: 10px;
 }
+#elevador{
+    position:absolute; 
+    top:40vh; 
+    left:-20vh;
+  }
+
 </style>
