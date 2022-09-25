633c86cdfccd07d244b7fa6f23f4e4f574e7f3c2
1Zod <nathanmarrega@gmail.com>
2022-09-25 15:16:19 -0300

    Add botoes elevador

--- src/views/MyElevador.vue
+++ src/views/MyElevador.vue
@@ -1,11 +1,13 @@
 <template>
     <div class="background-elevador">
         <div id="principal">
-            <div class="campo_predio">
-            <img src="@/assets/img/predio.png" style="height: 99vh;">
-            </div>
-            <div class="campo_elevador">
-            <img id="elevador" src="@/assets/img/elevador.png" style="height: 99vh;">
+            <div class="campo-predio">
+              <img src="@/assets/img/predio.png">
+              <img id="elevador" src="@/assets/img/elevador.png">
+              <button type="button" @click="3"><img alt="3 andar" src="@/assets/img/3botao.png" style="height: 10vh;"/></button>
+              <button type="button" @click="2"><img alt="2 andar" src="@/assets/img/2botao.png" style="height: 10vh;"/></button>
+              <button type="button" @click="1"><img alt="1 andar" src="@/assets/img/1botao.png" style="height: 10vh;"/></button>
+              <button type="button" @click="terro"><img alt="terreo" src="@/assets/img/terreo.png" style="height: 10vh;"/></button>
             </div>
         </div>
     </div>
@@ -42,10 +44,14 @@ html, body {
     /* background-repeat: repeat; */
 }
 
+.campo-predio > img{
+    height: 99vh;
+}
+
+
 #principal{
     display: grid;
     grid-template-columns: 1vh 1vh 1vh 1vh;
-   
     justify-items: left;
     column-gap: 10px;
 }
@@ -53,6 +59,17 @@ html, body {
     position:absolute; 
     top:40vh; 
     left:-20vh;
+}
+
+.campo-predio> button{
+    position:relative;
+    display: flex; 
+    padding-bottom: 5vh;
+    top:-80vh; 
+    left:130vh;
+    border: none;
+    background-color: transparent;
+    cursor: pointer;
   }
 
 </style>
